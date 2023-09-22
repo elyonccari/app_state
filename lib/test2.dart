6d953ee1009739af5777e176fb39be72b8de6d7f
elyon <elyonccari7692@gmail.com>
2023-09-22 18:21:26 -0500

    primer comit

---
+++ lib/test2.dart
@@ -0,0 +1,47 @@
+import 'package:flutter/material.dart';
+
+class Test2Page extends StatefulWidget {
+  const Test2Page({super.key});
+
+  @override
+  State<Test2Page> createState() => _Test2PageState();
+}
+
+class _Test2PageState extends State<Test2Page> {
+  bool isTitleBold = true;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Estilos"),
+      ),
+      body: Padding(
+        padding: EdgeInsets.all(10.0),
+        child: Column(
+          children: [
+            Text(
+              "Pokemon",
+              style: TextStyle(
+                fontSize: 20.0,
+                fontWeight: 
+                isTitleBold == true? FontWeight.bold : FontWeight.normal,
+
+              ),
+            ),
+            Text(
+              "Phasellus lacinia accumsan odio, non porta lorem mattis vel. Mauris volutpat viverra interdum. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Vivamus hendrerit, felis in bibendum commodo, tellus libero sollicitudin mi, ac suscipit massa dui eget nisl. Vivamus maximus lectus vel massa ornare vulputate. ",
+            ),
+            Checkbox(
+                value: isTitleBold,
+                onChanged: (bool? value) {
+                  isTitleBold = value!;
+                  print(value);
+                  setState(() {});
+                }),
+          ],
+        ),
+      ),
+    );
+  }
+}
